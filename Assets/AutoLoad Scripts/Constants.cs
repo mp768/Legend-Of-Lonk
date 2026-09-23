e139fd9f897005e7c4ed4ce2d7082af28d1e219d
mp768 <mp7252006@gmail.com>
2026-09-23 12:09:25 -0500

    Add Lonk Movement
    - Just need to add animation and various other things.

---
+++ Assets/AutoLoad Scripts/Constants.cs
@@ -0,0 +1,4 @@
+public static class Constants
+{
+    public const string COLLIDING_OBJECT_GROUP_TAG = "walls";
+}
--- Assets/Scenes/Lonk.cs
+++ Assets/Scenes/Lonk.cs
@@ -1,45 +1,144 @@
 using Godot;
 using System;
 
-public partial class Lonk : CharacterBody2D
+public partial class Lonk : Area2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	private enum MovementDirection
+	{
+		HORIZONTAL,
+		VERTICAL,
+	}
+
+	private Vector2I RAY_LENGTH = new(6, 3);
+
+	private AnimatedSprite2D sprite;
+	private Vector2 footPostitionOffset;
+
+	private MovementDirection previousMovementDirection = MovementDirection.HORIZONTAL;
+
+	// I recorded how many frames it took for Link's movement sprite to update and it
+	// was every 6 movement frames.
+	private const int MOVEMENT_SUB_ANIMATION_FRAME_MAX = 6;
+	private int movementSubAnimationFrame = 0;
+
+	public override void _Ready()
+	{
+		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		var footPositionNode = GetNode<Node2D>("Foot Position");
+		footPostitionOffset = footPositionNode.Position;
+
+		// We don't need this marker to live after we've gotten this information.
+		footPositionNode.QueueFree();
+	}
+
+	private Vector2I GetInputDirection()
+	{
+		return new Vector2I(
+			(int)(Input.GetActionStrength("right") - Input.GetActionStrength("left")),
+			(int)(Input.GetActionStrength("down") - Input.GetActionStrength("up"))
+		);
+	}
+
+	private (bool, bool) GetDirectionRaycasts(Vector2I direction)
+	{
+		var spaceState = GetWorld2D().DirectSpaceState;
+
+		var position = GlobalPosition + footPostitionOffset;
+
+		var verticalQuery = PhysicsRayQueryParameters2D.Create(
+			position,
+			position + new Vector2(0, direction.Y * RAY_LENGTH.Y)
+		);
+
+		var horizontalQuery = PhysicsRayQueryParameters2D.Create(
+			position,
+			position + new Vector2(direction.X * RAY_LENGTH.X, 0)
+		);
+
+		var verticalResult = spaceState.IntersectRay(verticalQuery);
+		var horizontalResult = spaceState.IntersectRay(horizontalQuery);
+
+		var hitVertical = false;
+		var hitHorizontal = false;
+
+		if (verticalResult.Count > 0)
+		{
+			var vCollider = verticalResult["collider"].As<CollisionObject2D>();
+
+			if (vCollider != null)
+				hitVertical = vCollider.IsInGroup(Constants.COLLIDING_OBJECT_GROUP_TAG);
+		}
+
+		if (horizontalResult.Count > 0)
+		{
+			var hCollider = horizontalResult["collider"].As<CollisionObject2D>();
+
+			if (hCollider != null)
+				hitHorizontal = hCollider.IsInGroup(Constants.COLLIDING_OBJECT_GROUP_TAG);
+		}
+
+		return (hitHorizontal, hitVertical);
+	}
+
+	private void RoundPositionToTile()
+	{
+		// Visual tiles on the NES were 8x8, while logical tiles for blocks of sprites
+		// were typically 16x16. However, in the LoZ if link changed to a different 
+		// axis of movement, he would always bind himself to the nearest visual tile,
+		// so 8x8.
+
+		Position = new Vector2(
+			Mathf.Round(Position.X / 8.0f) * 8.0f,
+			Mathf.Round(Position.Y / 8.0f) * 8.0f
+		);
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
-		Vector2 direction = Vector2.Zero;
-		//up + down has priority over left/right
+		var inputDirection = GetInputDirection();
 
-		int up = Input.IsActionPressed("up") ? 1 : 0 ;
-		int down = Input.IsActionPressed("down") ? 1: 0;
-		int left = Input.IsActionPressed("left") ? 1: 0;
-		int right = Input.IsActionPressed("right") ? 1: 0;
+		var (hHit, vHit) = GetDirectionRaycasts(inputDirection);
 
-		// up || down	
-		if (up + down >= 1) // int equivalent of booleans where at least one of them is true
+		// Vertical movement has priority until Link is stuck hitting
+		// a wall.
+		if (!vHit && inputDirection.Y != 0)
 		{
-			direction += Vector2.Up * up + Vector2.Down * down;
-		// left || right
-		} else if (left + right >= 1) {
-			direction += Vector2.Left * left + Vector2.Right * right;
-		}
+			// In the original game, when you switch your axis of movement,
+			// Link's position gets locked to the center of a nearby
+			// 8x8 tile.
+			if (previousMovementDirection != MovementDirection.VERTICAL)
+			{
+				sprite.Frame = 0;
+				RoundPositionToTile();
+				previousMovementDirection = MovementDirection.VERTICAL;
+			}
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Y = direction.Y * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+			// Reminder: Positive is down in Godot.
+			if (inputDirection.Y > 0)
+			{
+				sprite.Animation = "walk_down";
+			}
+			else
+			{
+				sprite.Animation = "walk_up";
+			}
 
-		Velocity = velocity;
-		MoveAndSlide();
+			Position += new Vector2(0, inputDirection.Y * 1.5f);
+		}
+		else if (!hHit && inputDirection.X != 0)
+		{
+			if (previousMovementDirection != MovementDirection.HORIZONTAL)
+			{
+				sprite.Frame = 0;
+				RoundPositionToTile();
+				previousMovementDirection = MovementDirection.HORIZONTAL;
+			}
+
+			sprite.Animation = "walk_horizontal";
+			sprite.FlipH = inputDirection.X < 0;
+
+			Position += new Vector2(inputDirection.X * 1.5f, 0);
+		}
 	}
 }
