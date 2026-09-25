1c4d09ae71d4e76d5d07f31d74d740fb05021b72
Mike P <mp7252006@gmail.com>
2026-09-25 15:06:14 -0500

    Update Door Unlock logic.
    - Door now unlocks using link's smaller collision box, rather than his
      hitbox. It makes it so he isn't opening it by slightly clippin the
      door's front with his head.

--- Assets/Scripts/LockedDoor.cs
+++ Assets/Scripts/LockedDoor.cs
@@ -8,15 +8,16 @@ public partial class LockedDoor : TileMapLayer
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_lockedDoor.AreaEntered += OnAreaEntered;
+		_lockedDoor.AreaEntered += (_) => OnPlayerEntered();
+		_lockedDoor.BodyEntered += (_) => OnPlayerEntered();
 	}
 
-	private async void OnAreaEntered(Area2D _)
+	private async void OnPlayerEntered()
 	{
 		// TODO: A check should be here to check if we have a key to open a door.
 
 		// A little timer that we set so that the door doesn't look like it opens up immediately.
-		await ToSignal(GetTree().CreateTimer(0.25f), SceneTreeTimer.SignalName.Timeout);
+		await ToSignal(GetTree().CreateTimer(0.05f), SceneTreeTimer.SignalName.Timeout);
 
 		QueueFree();
 	} 
