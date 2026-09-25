47b83de8ac9ed6a7cce0c2cbe04237ffeb142fde
Mike P <mp7252006@gmail.com>
2026-09-25 09:29:24 -0500

    Separate Animation from Lonk
    - Now we can reuse that for other entities in the game, like Stalfos

--- Assets/Scenes/Lonk.cs
+++ Assets/Scenes/Lonk.cs
@@ -1,52 +1,20 @@
 using Godot;
-using System;
 
 public partial class Lonk : CharacterBody2D
 {
     [Export] private GridMovementComponent _movementComponent;
-    [Export] private Vector2I spriteOffset;
-    [Export] private AnimatedSprite2D _sprite;
-
-    private int _movementFrameCounter = 0;
-    private const int ANIMATION_STEP_FREQUENCY = 6;
+    [Export] private GridAnimationComponent _animationComponent;
 
     private enum Axis { None, Horizontal, Vertical }
     private Axis _primaryAxis = Axis.None;
-
-    public override void _Ready()
-    {
-        if (_sprite != null)
-        {
-            _sprite.Stop(); 
-        }
-    }
 
     public override void _PhysicsProcess(double delta)
     {
         float fDelta = (float)delta;
         Vector2 inputDir = GetCardinalInput(fDelta);
 
-        // Delegate the actual movement & snapping to the component
-        _movementComponent.Move(inputDir, delta);
-
-        if (inputDir != Vector2.Zero)
-        {
-            UpdateFacingAndAnimation(inputDir);
-            AdvanceAnimationFrame();
-        }
-        else
-        {
-            _movementFrameCounter = 0;
-        }
-
-        // Keep visuals fixed while not messing with the physics position.
-        if (_sprite != null)
-        {
-            _sprite.Position = new Vector2(
-                Mathf.Round(Position.X) - Position.X + spriteOffset.X,
-                Mathf.Round(Position.Y) - Position.Y + spriteOffset.Y
-            );
-        }
+        _movementComponent?.Move(inputDir, delta);
+        _animationComponent?.UpdateAnimation(inputDir);
     }
 
     private Vector2 GetCardinalInput(float delta)
@@ -66,7 +34,7 @@ public partial class Lonk : CharacterBody2D
         Vector2 vertDir = hasY ? new Vector2(0.0f, Mathf.Sign(y)) : Vector2.Zero;
         Vector2 horzDir = hasX ? new Vector2(Mathf.Sign(x), 0.0f) : Vector2.Zero;
 
-        if (hasY && hasX)
+        if (hasY && hasX && _movementComponent != null)
         {
             if (_primaryAxis == Axis.Horizontal)
             {
@@ -75,7 +43,7 @@ public partial class Lonk : CharacterBody2D
                 {
                     return horzDir;
                 }
-                return vertDir; 
+                return vertDir;
             }
             else if (_primaryAxis == Axis.Vertical)
             {
@@ -84,7 +52,7 @@ public partial class Lonk : CharacterBody2D
                 {
                     return vertDir;
                 }
-                return horzDir; 
+                return horzDir;
             }
         }
 
@@ -96,41 +64,5 @@ public partial class Lonk : CharacterBody2D
 
         _primaryAxis = Axis.Horizontal;
         return horzDir;
-    }
-
-    private void UpdateFacingAndAnimation(Vector2 inputDir)
-    {
-        if (inputDir.X > 0)
-        {
-            _sprite.Animation = "walk_horizontal";
-            _sprite.FlipH = false;
-        }
-        else if (inputDir.X < 0)
-        {
-            _sprite.Animation = "walk_horizontal";
-            _sprite.FlipH = true;
-        }
-        else if (inputDir.Y > 0)
-        {
-            _sprite.Animation = "walk_down";
-        }
-        else if (inputDir.Y < 0)
-        {
-            _sprite.Animation = "walk_up";
-        }
-    }
-
-    private void AdvanceAnimationFrame()
-    {
-        _movementFrameCounter++;
-        if (_movementFrameCounter >= ANIMATION_STEP_FREQUENCY)
-        {
-            _movementFrameCounter = 0;
-            int frameCount = _sprite.SpriteFrames.GetFrameCount(_sprite.Animation);
-            if (frameCount > 0)
-            {
-                _sprite.Frame = (_sprite.Frame + 1) % frameCount;
-            }
-        }
     }
 }
